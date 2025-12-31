5672f41c1b0bd749b94786f4bf62fb8b04a283c4
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-12-31 22:40:06 +0300

    Add files via upload

---
+++ TestJenkins/TestJenkins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Engin Gülek on 31.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestJenkins/TestJenkinsUITests/TestJenkinsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TestJenkinsUITestsLaunchTests.swift
+//  TestJenkinsUITests
+//
+//  Created by Engin Gülek on 31.12.2025.
+//
+
+import XCTest
+
+final class TestJenkinsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
